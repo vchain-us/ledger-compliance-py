11a468d0b6ea9a11ef6a3cdb9ed8a1663764e7c5
Simone Lazzaris <slazzaris@gmail.com>
2020-10-20 11:55:05 +0200

    Added examples

---
+++ examples/batchinsert.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+API="qervegelclrmbvcdhpvvmebpnyxgjknysayt"
+HOST="172.31.255.30"
+PORT=3324
+BATCHSIZE=500
+NUM=20
+
+import LedgerCompliance.client
+import string
+import random
+import time
+
+cli=LedgerCompliance.client.Client(API, HOST, PORT)
+cli.connect()
+
+def get_random_string(length):
+    letters = string.ascii_lowercase
+    ret = ''.join(random.choice(letters) for i in range(length))
+    return ret
+
+print("Generating")
+manyvalues=[]
+for i in range(0,NUM):
+	manyvalues.append({})
+	for j in range(0,BATCHSIZE):
+		k="KEY_"+get_random_string(8)
+		v="VALUE_"+get_random_string(64)
+		manyvalues[i][k.encode('ascii')]=v.encode('ascii')
+print("Starting insert")
+t0=time.time()
+for i in range(0,NUM):
+	cli.setBatch(manyvalues[i])
+t1=time.time()
+if t1!=t0:
+	insert_sec=(BATCHSIZE*NUM)/(t1-t0)
+else:
+	insert_sec=0 # avoid dividing per zero
+print("Inserted {} values in {:.3f} seconds {:.3f} insert/s".format(BATCHSIZE*NUM, t1-t0, insert_sec))
+
+
---
+++ examples/history.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+API="qervegelclrmbvcdhpvvmebpnyxgjknysayt"
+HOST="172.31.255.30"
+PORT=3324
+
+import LedgerCompliance.client
+import string
+import random
+import time
+
+cli=LedgerCompliance.client.Client(API, HOST, PORT)
+cli.connect()
+
+def get_random_string(length):
+    letters = string.ascii_lowercase
+    ret = ''.join(random.choice(letters) for i in range(length))
+    return ret
+
+KEY="LC_history".encode('ascii')
+for i in range(0,10):
+	cli.set(KEY, get_random_string(16).encode('ascii'))
+
+ret=cli.history(KEY)
+for h in ret:
+	print("{:d}: {:s}".format(h.index, h.value.decode('ascii')))
